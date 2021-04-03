656dfdf8c399b37dd51dcd6cd3aba12b13ff682c
Sheridan McKisick <mcksheridan@gmail.com>
2021-04-03 08:32:08 -0500

    Add hero data

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,10 +9,10 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hello, my name is',
+  name: 'Sheridan',
+  subtitle: "I'm a Full Stack Developer",
+  cta: 'Learn more',
 };
 
 // ABOUT DATA
