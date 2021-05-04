403fcb8657534c074b49fa202534e84b45bf1107
Sheridan McKisick <mcksheridan@gmail.com>
2021-05-04 15:52:17 -0500

    Create bold label for Languages/Tools

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -47,7 +47,9 @@ const Projects = () => {
                           {info ||
                             'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
                         </p>
-                        <p className="mb-4">Languages/Tools: {info2 || ''}</p>
+                        <p className="mb-4">
+                          <span className="mb-4__label">Languages/Tools:</span> {info2 || ''}
+                        </p>
                       </div>
                       <a
                         target="_blank"
