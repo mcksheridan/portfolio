485a2b1facc518d891dfa6df4d6a36e85205c303
Sheridan McKisick <sheridan@heysparkbox.com>
2023-11-28 15:32:09 -0500

    feat: style social media

--- src/components/socialMedia.tsx
+++ src/components/socialMedia.tsx
@@ -3,12 +3,14 @@ import type { SocialMediaCollectionProps } from "../types";
 
 const SocialMedia = ({ socialMedia }: SocialMediaCollectionProps) => {
   return (
-    <div>
-      <ul>
+    <div className="social-media">
+      <ul className="social-media__container">
         {socialMedia.map((media) => {
           return (
             <li key={media.title}>
-              <a href={media.url} aria-label={media.icon ? media.title : ''}>{media.icon ?? media.title}</a>
+              <a href={media.url} aria-label={media.icon ? media.title : ''}>
+                {media.icon ? <img src={`/${media.icon}`} height="50" alt="" /> : media.title}
+              </a>
             </li>
           )
         })}
--- src/data/socialMedia.ts
+++ src/data/socialMedia.ts
@@ -3,19 +3,23 @@ import type { SocialMediaProps } from "../types"
 const SOCIAL_MEDIA_DATA: SocialMediaProps[] = [
   {
     title: 'Email',
-    url: 'mailto:mcksheridan@gmail.com'
+    url: 'mailto:mcksheridan@gmail.com',
+    icon: 'social__email.png'
   },
   {
     title: 'CodePen',
-    url: 'https://codepen.io/mcksheridan'
+    url: 'https://codepen.io/mcksheridan',
+    icon: 'social__codepen.png'
   },
   {
     title: 'LinkedIn',
-    url: 'https://www.linkedin.com/in/sheridanm/'
+    url: 'https://www.linkedin.com/in/sheridanm/',
+    icon: 'social__linkedin.png'
   },
   {
     title: 'GitHub',
-    url: 'https://github.com/mcksheridan'
+    url: 'https://github.com/mcksheridan',
+    icon: 'social__github.png'
   }
 ]
 
