7c23e103b078096797d4e9486810e3119c092f50
Sheridan McKisick <sheridan@heysparkbox.com>
2023-11-28 14:57:13 -0500

    feat: style projects

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -3,22 +3,20 @@ import type { ProjectProps } from "../types";
 
 const Project = ({ title, image, description, techStack, links }: ProjectProps) => {
   return (
-    <article>
-      <div>
-        <h3>{title}</h3>
+    <article className="project">
+      <h3>{title}</h3>
+        <figure className="project__image-wrapper">
+          <img src={`/${image}`} alt={`Screenshot of ${title}`} className="project__image" />
+        </figure>
         <p>{description}</p>
-        <h4>Languages/Tools</h4>
-        <ul>
-          {techStack.map((tech, i) => <li key={i}>{tech}</li>)}
+        <h4 className="project__subheading">Languages/Tools</h4>
+        <ul className="techstack">
+          {techStack.map((tech, i) => <li key={i} className="techstack__tech">{tech}</li>)}
         </ul>
-        <div>
-          {links.live && <a href={links.live}>See Live</a>}
-          <a href={links.code}>Source Code</a>
+        <div className="project__links">
+          {links.live && <a href={links.live} className="link-button">See Live</a>}
+          <a href={links.code} className="link">Source Code</a>
         </div>
-      </div>
-      <div>
-        <figure><img src={`/${image}`} alt={`Screenshot of ${title}`} /></figure>
-      </div>
     </article>
   )
 }
