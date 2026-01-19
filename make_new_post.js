de3d4b314431b10205da7d2fc35845064db3d79c
Sheridan Mehta-McKisick <mcksheridan@gmail.com>
2026-01-19 11:21:16 -0500

    chore: improve post generator
    Allows titles with spaces
    
    Easier access to post file
    
    Accounts for new folder structure

--- make_new_post.js
+++ make_new_post.js
@@ -1,13 +1,16 @@
 const fs = require('node:fs')
+const path = require('path')
 
 const now = new Date()
+const year = now.getFullYear()
 const month = now.getMonth() + 1
-const date = `${now.getFullYear()}-${month < 9 ? '0' + month : month}-${now.getDate()}`
-const title = process.argv.slice(2)[0]
+const monthString = month < 9 ? '0' + month : month
+const date = `${year}-${monthString}-${now.getDate()}`
+const title = process.argv.slice(2)
 
 const content = (
   '---\n' +
-  `title: ${title}\n` + 
+  `title: ${title.join(' ')}\n` + 
   'layout: layouts/post.njk\n' +
   'tags: []\n' +
   `date: ${date}\n` +
@@ -16,10 +19,14 @@ const content = (
   '---\n\n'
 )
 
-fs.writeFile(`src/posts/${date}--${title.toLowerCase()}.md`, content, err => {
+const fileName = `src/posts/${year}/${monthString}/${title.join('-').toLowerCase()}.md`
+
+fs.mkdirSync(path.dirname(fileName), { recursive: true })
+
+fs.writeFile(fileName, content, err => {
   if (err) {
     console.error('An error has occured: ', { err })
   } else {
-    console.log('File successfully created!')
+    console.log('File successfully created! ', fileName)
   }
 });
